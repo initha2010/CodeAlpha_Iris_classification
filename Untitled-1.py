400e7b6b1fe48eaf2ab9201814b66f2be8b8e37c
initha2010 <erinithav@gmail.com>
2026-07-13 19:25:28 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv("data.csv")
+X = df.drop("label", axis=1)
+y = df["label"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = DecisionTreeClassifier(max_depth=3, random_state=42)
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+print(accuracy_score(y_test, pred))
